6848739794b809af72973f9e87d734f5542df05a
Jeffrey Long <jlong@groupon.com>
2014-07-01 12:04:20 -0500

    Accept error callback; Make both callbacks optional

--- jaxx.js
+++ jaxx.js
@@ -1,18 +1,21 @@
 window.Jaxx = (function(){
-  var createRequest = function(method, url, callback){
+  var createRequest = function(method, url, successCb, errorCb){
     var request = new XMLHttpRequest();
-    request.onload = callback;
+    successCb = successCb || function(){};
+    errorCb = errorCb || function(){};
+    request.addEventListener('load', successCb, false);
+    request.addEventListener('error', errorCb, false);
     request.open(method, url, true);
     request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
     return request;
   };
   return {
-    get: function(url, callback){
-      var request = createRequest('get', url, callback);
+    get: function(url, success, error){
+      var request = createRequest('get', url, success, error);
       request.send();
     },
-    post: function(url, payload, callback){
-      var request = createRequest('post', url, callback);
+    post: function(url, payload, success, error){
+      var request = createRequest('post', url, success, error);
       request.setRequestHeader('Content-Type', 'application/json');
       request.send(JSON.stringify(payload));
     }
