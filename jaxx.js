896c4f08ee9d363741a8c4f3daef4234a75e709e
Jeffrey Long <jlong@groupon.com>
2014-07-03 11:39:56 -0500

    Don't register events if callback doesn't exist; remove one level of abstraction

--- jaxx.js
+++ jaxx.js
@@ -1,20 +1,20 @@
 window.Jaxx = (function(){
   var createRequest = function(method, url, successCb, errorCb){
     var request = new XMLHttpRequest();
-    registerCallbacks(request, successCb, errorCb);
+
+    registerCallback(request, 'load', successCb);
+    registerCallback(request, 'error', errorCb);
+
     request.open(method, url, true);
     request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
     return request;
   };
 
-  var registerCallbacks = function(request, successCb, errorCb){
-    registerCallback(request, 'load', successCb);
-    registerCallback(request, 'error', errorCb);
-  };
-
   var registerCallback = function(request, eventType, callback){
-    request.addEventListener(eventType, callback || function(){}, false);
-  }
+    if(callback){
+      request.addEventListener(eventType, callback, false);
+    }
+  };
 
   return {
     get: function(url, success, error){
